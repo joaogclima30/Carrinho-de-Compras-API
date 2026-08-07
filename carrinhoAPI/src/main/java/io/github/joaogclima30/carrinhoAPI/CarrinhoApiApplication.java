44376f3963cb1f2acc2ebd59fdf1f048033e3bab
João Gabriel <jg.lima2608@gmail.com>
2026-08-07 13:40:58 -0300

    Commit Test

---
+++ carrinhoAPI/src/main/java/io/github/joaogclima30/carrinhoAPI/CarrinhoApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaogclima30.carrinhoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoApiApplication.class, args);
+	}
+
+}
---
+++ carrinhoAPI/src/test/java/io/github/joaogclima30/carrinhoAPI/CarrinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaogclima30.carrinhoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
